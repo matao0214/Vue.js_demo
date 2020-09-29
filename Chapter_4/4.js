427c956c7fc518e4c9b113db2380afdd1e81c9f9
matao0214 <y.kawamata0214@gmail.com>
2020-09-29 23:43:02 +0900

    until_p128

--- Chapter_4/4.js
+++ Chapter_4/4.js
@@ -2,10 +2,39 @@ new Vue ({
   el: "#app",
   data: {
     width: 800,
-    height: 600
+    height: 600,
+    budget: 300,
+    limit: 2,
+    list: [
+      { id: 1, name: 'りんご', price: 100 },
+      { id: 2, name: 'ばなな', price: 200 },
+      { id: 3, name: 'いちご', price: 400 },
+      { id: 4, name: 'おれんじ', price: 300 },
+      { id: 5, name: 'メロン', price: 500 },
+    ],
+    order: false
   },
 
   computed: {
+    // budget以下のリストを返す算出プロパティ
+    matched: function() {
+      return this.list.filter(function(el){
+        return el.price <= this.budget
+      }, this)
+    },
+
+    // sorted: function() {
+    //   return _.orderBy(this.matched, 'price', this.order ? 'desc' : 'asc')
+    // },
+
+    // matchedで返ったデータをlimit件で返す算出プロパティ
+    limited: function() {
+      return this.matched.slice(0, this.limit)
+    },
+
+
+
+
     // 算出プロパティhalfWidthを定義
     halfWidth: {
       get: function() { return this.width / 2 },
